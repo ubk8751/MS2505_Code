52365ecf1ae3660d0068f25951226538db919d19
ejmsona <samuel.b.jonsson@ericsson.com>
2024-12-12 11:17:00 +0100

    deleted lecture code

--- LectureCode/BayesianUpdatingProcess.r
+++
@@ -1,6 +0,0 @@
-# Define prior distribution
-prior <- rbeta(1000, 1, 1)
-# Update with observed data
-posterior <- dbeta(seq(0, 1, length = 100), 6, 6)
-# Plot results
-plot(seq(0, 1, length = 100), posterior, type = "l")
--- LectureCode/Example1_Bayes.r
+++
@@ -1,8 +0,0 @@
-# Defining probabilities
-p_cancer <- 0.05
-p_positive_given_cancer <- 0.78
-p_positive <- 0.096
-# Applying Bayes’ Rule
-p_cancer_given_positive <- (p_positive_given_cancer * p_cancer) / p_positive
-# Result
-print(p_cancer_given_positive)
